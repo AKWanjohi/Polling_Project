3ac8ee8196f81588f5e891242b5ea0d90a50c7ef
Alvin Wanjohi <akwanjohi99@gmail.com>
2021-03-30 16:56:45 +0300

    Created some automated tests.
    -  Improved the index and detail view.
    - Added a function to the Question model.
    - Update '.gitignore' file.

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(
+        Question, pk=question_id, pub_date__lte=timezone.now())
     context = {'question': question}
     return render(request, 'polls/detail.html', context)
 
